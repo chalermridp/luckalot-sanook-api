e41becc7da858ed2c952cfb0f5d2ede8f4eb8b94
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2021-01-12 22:02:00 +0700

    udpate deploy command

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,5 @@ npm run build
 npm prune --production
 zip -r artifact/luckalot-sanook-api.zip dist/ node_modules
 
-export AWS_PROFILE=luckalot.${StageName:-"dev"}
-aws cloudformation package --template-file template.yaml --s3-bucket luckalot-sanook-api.${StageName:-"dev"}-artifact --output-template-file artifact/template.${StageName:-"dev"}.out.yaml
-aws cloudformation deploy --template-file artifact/template.${StageName:-"dev"}.out.yaml --stack-name luckalot-sanook-api-${StageName:-"dev"}-stack --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package --template-file template.yaml --s3-bucket luckalot-sanook-api-artifact --output-template-file artifact/template.${StageName:-"dev"}.out.yaml --profile luckalot.${StageName:-"dev"}
+aws cloudformation deploy --template-file artifact/template.${StageName:-"dev"}.out.yaml --stack-name luckalot-sanook-api-stack --capabilities CAPABILITY_NAMED_IAM --profile luckalot.${StageName:-"dev"}
